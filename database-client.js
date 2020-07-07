f27ab5dded1e817b81005fa22f916f6fba10da0d
Keian Rao <keian.rao@gmail.com>
2020-07-07 19:43:15 -0400

    Added in skeleton for submit page. Set connectToDatabase to return a placeholder Promise.

---
+++ database-client.js
@@ -0,0 +1,10 @@
+
+databaseURL = "localhost:8080"
+
+function connectToDatabase()
+{
+    // We should return a Promise here.
+    // Also, start doing the backend please. We need to be testing things as we go.
+
+    return Promise.resolve(true);
+}
