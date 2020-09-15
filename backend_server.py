7fef920a623fcdfbc5f74a1a8c60d0a214c564f8
Keian Rao <keian.rao@gmail.com>
2020-09-15 09:13:16 -0400

    Added basic JSON handling in POST route

--- backend_server.py
+++ backend_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, jsonify
+from werkzeug.exceptions import BadRequest
 app = Flask(__name__)
 
 
@@ -13,25 +14,31 @@ def index():
 		"argumentType": "dictionary"
 	});
 
-@app.route('/', methods = ["PUT"])
+@app.route('/', methods = ["POST"])
 def submit():
-	print(request.form)
-	# Flask docs just says that this is a dict with 'parsed form data'
-	# from POST or PUT requests. But where is it coming from, and
-	# what counts as "form data" in this case?
-	# Meanwhile, request.args is what I thought this was, the parsed
-	# a parsed query string if available.
-	# Right now, our plan is to have requests give JSON data inside
-	# their body. We should use request.get_json for that case.
-	
-	# About 'request': https://tedboy.github.io/flask
-	# /interface_api.incoming_request_data.html#flask.request
-	# It is a proxy, that correctly gets the data for
-	# the particular thread accessing it.
-	return jsonify({
+	if not request.is_json:
+		response = jsonify({
+			"error": "Input data must be in the form of JSON!" \
+				" Check that the request mimetype is 'application/json'."
+		});
+		response.status_code = 400;
+		return response;
+
+	try:
+		input_data = request.get_json();
+	except BadRequest:
+		response = jsonify({
+			"error": "Input data must be in the form of JSON!" \
+				" Check that the data in the request is well-formed JSON."
+		});
+		response.status_code = 400;
+		return response;
+
+	response = jsonify({
 		"error": "Operation not supported yet!"
 	});
-	# Please figure out how to give a HTTP error.
+	response.status_code = 500;
+	return response;
 
 
 ##	%%	##	%%	##	%%	##	%%	##
@@ -42,11 +49,11 @@ if __name__ == "__main__":
 	if not db.initialise_database_connection_from_default_config_file():
 		print("{}{}{}{}".format(
 			"We tried to read database connection settings from ",
-			"the config file $HOME/.config/KayaBlog/connection, ",
+			"the config file $HOME/.config/KayaBlog1/connection, ",
 			"but that didn't go well. Could you check the file? ",
 			"If it doesn't exist, please fill it out. Thanks!"
 		));
 		exit(1);
-		
+
 	app.run();
 	db.disconnect_database_connection();
