d8a25023fc27356fadcaf4661b02bbf9b64046e1
Keian Rao <keian.rao@gmail.com>
2020-10-03 17:37:25 -0400

    Filled a bit of backendclient.js. Stopped at CORS barrier

--- backendclient.js
+++ backendclient.js
@@ -1,10 +1,27 @@
 
-databaseURL = "localhost:8080"
+const serverURL = "http://localhost:5000";
 
-function connectToDatabase()
+function connectToServer()
 {
     // We should return a Promise here.
-    // Also, start doing the backend please. We need to be testing things as we go.
-
-    return Promise.resolve(true);
+    // Also, start doing the backend please. 
+	// We need to be testing things as we go.
+	fetch(serverURL)
+		.then(function (response) {
+			response.json()
+				.then(function (json) {
+					console.log(json);
+					return true;
+				})
+				.catch(function (error) {
+					console.log(json);
+					return false;
+				});
+		})
+		.catch(function (error) {
+			console.log(error);
+			return false;
+		});
 }
+
+connectToServer();
